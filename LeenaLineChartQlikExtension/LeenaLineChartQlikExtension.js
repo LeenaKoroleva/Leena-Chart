d82188f619b9542bf83b7fa3fe3ddd8153ae396d
Rodionov Sergey <36378925+Rodionov-Sergey@users.noreply.github.com>
2020-03-21 17:12:58 +0300

    * Получение данных линейной диаграммы из Qlik. * Добавлены JSDoc-определения данных для Qlik

--- LeenaLineChartQlikExtension/LeenaLineChartQlikExtension.js
+++ LeenaLineChartQlikExtension/LeenaLineChartQlikExtension.js
@@ -78,10 +78,24 @@ define(
 			 * @returns {Promise} Promise завершения отрисовки
 			 */
 			paint: function ($element, qlikLayout) {
+				try {
+					// DEBUG:
+					console.log('Данные Qlik: ', qlikLayout);
 
-				console.log(qlikLayout);
+					$element.html( "Линейный график Leena Chart" );
 
-				$element.html( "Линейный график Leena Chart" );
+					// Получение данных для графика
+					var chart = getLineChartData(qlikLayout);
+
+					// TODO: Отрисовка графика
+					// DEBUG:
+					console.log('Данные графика:', chart);
+
+                }
+                catch (error) {
+                    console.log(error);
+					throw error;
+                }
 
 				return qlik.Promise.resolve();
 			}
@@ -89,6 +103,129 @@ define(
 		};
 
 		return extensionModule;
+
+		/**
+		 * Возвращает данные для линейного графика
+		 * @param {QlikLayout} qlikLayout Данные расширения Qlik
+		 * @returns {LineChart} График
+		 */
+		function getLineChartData(qlikLayout) {
+			
+			// Строки данных Qlik
+			var rows = qlikLayout.qHyperCube.qDataPages[0].qMatrix;
+			// Столбцы данных Qlik
+			var columns = qlikLayout.qHyperCube.qDimensionInfo
+				.concat(qlikLayout.qHyperCube.qMeasureInfo);
+
+			// Линии
+			/** @type {LineChartLine[]} */
+			var lines = [];
+			for (var columnIndex = 1; columnIndex < columns.length; columnIndex++) {
+				var line = getLineChartLine(rows, columns, 0, columnIndex);
+				lines.push(line);
+			}
+
+			// Список аргументов
+			var arguments = lines[0].points.map(
+				function(point) {
+					return point.argument;
+				}
+			);
+
+			// График
+			/** @type {LineChart} */
+			var chart = {
+				lines: lines,
+				arguments: arguments
+			};
+			return chart;
+		}
+
+		/**
+		 * Возвращает точку линейного графика для строки данных
+		 * @param {NxCell[][]} rows Строки данных
+		 * @param {(NxDimension|NxMeasure)[]} columns Столбцы данных
+		 * @param {Number} argumentColumnIndex Индекс столбца аргумента
+		 * @param {Number} valueColumnIndex Индекс столбца значения
+		 * @returns {LineChartPoint} Точка графика
+		 */
+		function getLineChartLine(rows, columns, argumentColumnIndex, valueColumnIndex) {
+
+			var linePoints = rows.map(
+				function (row) {
+					return getLineChartPoint(row, argumentColumnIndex, valueColumnIndex);
+				}
+			);
+
+			/** @type {LineChartLine} */
+			var line = {
+				title: columns[valueColumnIndex].qFallbackTitle,
+				points: linePoints
+			};
+			return line;
+		}
+
+		/**
+		 * Возвращает точку линейного графика для строки данных
+		 * @param {NxCell[]} row Данные строки
+		 * @param {Number} argumentColumnIndex Индекс столбца аргумента
+		 * @param {Number} valueColumnIndex Индекс столбца значения
+		 * @returns {LineChartPoint} Точка графика
+		 */
+		function getLineChartPoint(row, argumentColumnIndex, valueColumnIndex) {
+			console.log('Text:', row[valueColumnIndex].qText, 'Number:', row[valueColumnIndex].qNum, 'Cell: ', row[valueColumnIndex]);
+			
+			var value = null;
+			if (!row[valueColumnIndex].qIsEmpty) {
+				if (row[valueColumnIndex].qNum === 'number') {
+					value = row[valueColumnIndex].qNum;
+				}
+			}
+
+			/** @type {LineChartPoint} */ 
+			var point = {
+				argument: row[argumentColumnIndex].qText,
+				value: value
+			};
+			return point;
+		}
 	}
 );
 
+/**
+ * JSDoc-определения для линейного графика Leena Chart
+ */
+
+/**
+ * Данные для построения линейного графика
+ * @typedef {Object} LineChart
+ * @property {LineChartLine[]} lines Линии графика
+ * @property {String[]} arguments Аргументы
+ */
+
+/**
+ * Линия линейного графика
+ * @typedef {Object} LineChartLine
+ * @property {String} title Заголовок линии
+ * @property {LineChartPoint[]} points Точки линии
+ */
+
+/**
+ * Точка линейного графика
+ * @typedef {Object} LineChartPoint
+ * @property {String} argument Категория 
+ * @property {Number=} value Значение
+ */
+
+
+ /**
+ * Данные расширения Qlik
+ * @typedef {Object} ExtensionCustomProperties
+ * @property {String} ext - Заголовок расширения
+ */
+
+/**
+ * Мера гиперкуба
+ * @typedef {Object} ColumnCustomProperties
+ * @property {String} col - Заголовок меры
+*/
