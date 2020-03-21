48013df98b56807ab1dc00baedfdf9429412742f
Rodionov Sergey <36378925+Rodionov-Sergey@users.noreply.github.com>
2020-03-21 13:00:10 +0300

    Добавлен шаблон расширения.

---
+++ LeenaChartQlikExtensionTemplate/LeenaChartQlikExtensionTemplate.js
@@ -0,0 +1,40 @@
+/**
+ * Шаблон расширения Qlik Sense для диаграммы Leena Chart
+ */
+define(
+	// Список зависимостей
+	[
+		// Qlik API
+		"qlik"
+	],
+
+	/**
+	 * Создаёт модуль расширения
+	 * @param {*} qlik Qlik API
+	 */
+	function (qlik) {
+		var extensionModule = {
+
+			support: {
+				snapshot: true,
+				export: true,
+				exportData : false
+			},
+
+			/**
+			 * Создаёт и обновляет интерфейс расширения
+			 * @param {*} $element Родительский jQuery-элемент
+			 */
+			paint: function ($element) {
+
+				$element.html( "Шаблон для создания диаграмм Leena Chart" );
+
+				return qlik.Promise.resolve();
+			}
+
+		};
+
+		return extensionModule;
+	}
+);
+
