71d921ae585928bb1919ae2390b9458d867e7655
Rodionov Sergey <36378925+Rodionov-Sergey@users.noreply.github.com>
2020-03-21 15:44:27 +0300

    Добавлены определения свойств для линейного графика.

--- LeenaLineChartQlikExtension/LeenaLineChartQlikExtension.js
+++ LeenaLineChartQlikExtension/LeenaLineChartQlikExtension.js
@@ -13,12 +13,58 @@ define(
 
 	/**
 	 * Создаёт модуль расширения
-	 * @param {*} qlik Qlik API
+	 * @param {QlikApi} qlik Qlik API
 	 * @param {*} $ jQuery API
 	 */
 	function (qlik, $) {
+
+		// Определения свойств
+		var propertyDefinitions = {
+			type: 'items',
+			component: 'accordion',
+			items: {
+				// Блок свойств Измерения
+				dimensions: {
+					uses: 'dimensions',
+					min: 1,
+					max: 1
+				},
+				// Блок свойств Меры
+				measures: {
+					uses: 'measures',
+					min: 1,
+					max: 10
+				},
+				// Блок свойств Сортировка
+				sorting: {
+					uses: 'sorting'
+				},
+				// Блок свойств Вид
+				settings: {
+					uses: 'settings'
+				}
+			}
+		};
+
+		// Модуль расширения Qlik Sense
 		var extensionModule = {
 
+			// Определения свойств
+			definition: propertyDefinitions,
+
+			// Настройки первичной загрузки данных
+			initialProperties: {
+				qHyperCubeDef: {
+					qDimensions: [],
+					qMeasures: [],
+					qInitialDataFetch: [{
+						qWidth: 11,
+						qHeight: 900
+					}]
+				}
+			},
+
+			// Настройки выгрузки
 			support: {
 				snapshot: true,
 				export: true,
@@ -28,8 +74,12 @@ define(
 			/**
 			 * Создаёт и обновляет интерфейс расширения
 			 * @param {*} $element Родительский jQuery-элемент
+			 * @param {QlikLayout} qlikLayout Данные расширения
+			 * @returns {Promise} Promise завершения отрисовки
 			 */
-			paint: function ($element) {
+			paint: function ($element, qlikLayout) {
+
+				console.log(qlikLayout);
 
 				$element.html( "Линейный график Leena Chart" );
 
