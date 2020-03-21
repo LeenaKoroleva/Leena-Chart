3b4024f70ccfc57c1409b609856d06e59bcc340c
Rodionov Sergey <36378925+Rodionov-Sergey@users.noreply.github.com>
2020-03-21 15:44:04 +0300

    * Добавлены определения свойств для шаблона. * Добавлен файл JSDoc-определений  для Qlik

--- LeenaChartQlikExtensionTemplate/LeenaChartQlikExtensionTemplate.js
+++ LeenaChartQlikExtensionTemplate/LeenaChartQlikExtensionTemplate.js
@@ -16,8 +16,52 @@ define(
 	 * @param {*} $ jQuery API
 	 */
 	function (qlik, $) {
+
+		// Определения свойств
+		var propertyDefinitions = {
+			type: 'items',
+			component: 'accordion',
+			items: {
+				// Блок свойств Измерения
+				dimensions: {
+					uses: 'dimensions',
+					min: 0
+				},
+				// Блок свойств Меры
+				measures: {
+					uses: 'measures',
+					min: 0
+				},
+				// Блок свойств Сортировка
+				sorting: {
+					uses: 'sorting'
+				},
+				// Блок свойств Вид
+				settings: {
+					uses: 'settings'
+				}
+			}
+		};
+
+		// Модуль расширения Qlik Sense
 		var extensionModule = {
 
+			// Определения свойств
+			definition: propertyDefinitions,
+
+			// Настройки первичной загрузки данных
+			initialProperties: {
+				qHyperCubeDef: {
+					qDimensions: [],
+					qMeasures: [],
+					qInitialDataFetch: [{
+						qWidth: 10,
+						qHeight: 1000
+					}]
+				}
+			},
+
+			// Настройки выгрузки
 			support: {
 				snapshot: true,
 				export: true,
@@ -27,6 +71,8 @@ define(
 			/**
 			 * Создаёт и обновляет интерфейс расширения
 			 * @param {*} $element Родительский jQuery-элемент
+			 * @param {QlikLayout} qlikLayout Данные расширения
+			 * @returns {Promise} Promise завершения отрисовки
 			 */
 			paint: function ($element) {
 
---
+++ LeenaChartQlikExtensionTemplate/qlik.jsdoc.js
@@ -0,0 +1,24 @@
+/**
+ * JSDoc-определения для Qlik API и кастомных свойств
+ */
+
+// Qlik API
+
+/**
+ * Qlik API
+ * @typedef {Object} QlikApi
+ * @property {PromiseApi} Promise - Точка доступа к Promise API
+ */
+
+ /**
+ * Promise API
+ * @typedef {Object} PromiseApi
+ * @property {function():Promise} resolve - Возвращает завершенный Promise
+ */
+
+ // Qlik API свойств и данных
+
+/**
+ * Данные расширения Qlik
+ * @typedef {Object} QlikLayout
+ */
